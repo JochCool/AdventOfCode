e2b09a291e3fcbbdd345dea5c3b7fc93c11239c7
Jochem <29899660+JochCool@users.noreply.github.com>
2023-12-14 22:05:30 +0100

    Add test for 2023 day 12 part 2

--- JochCool.AdventOfCode.Test/Year2023/Day12.cs
+++ JochCool.AdventOfCode.Test/Year2023/Day12.cs
@@ -11,4 +11,12 @@ public static class Day12
 	{
 		Assert.Equal(expected, Part1.Solve(input));
 	}
+
+	[Theory]
+	[PuzzleData("525152", "day12_simple.txt")]
+	[PuzzleData("850504257483930", "day12.txt")]
+	public static void TestPart2(string expected, TextReader input)
+	{
+		Assert.Equal(expected, Part2.Solve(input));
+	}
 }
